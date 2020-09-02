aee75882cd1bc725b1670f05c583a96f6215dfb9
Jordan Roberts <jdrobs19@gmail.com>
2020-09-01 21:10:21 -0600

    filter by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ let userFormEl = document.querySelector("#user-form");
 let nameInputEl = document.querySelector("#username");
 let repoContainerEl = document.querySelector("#repos-container");
 let repoSearchTerm = document.querySelector("#repo-search-term");
+let languageButtonsEl = document.querySelector("#language-buttons");
 
 let getUserRepos = function(user){
     //format the github api url
@@ -84,3 +85,28 @@ let displayRepos = function(repos, searchTerm){
         repoEl.appendChild(statusEl);
     }
 };
+
+let getFeaturedRepos = function(language){
+ let apiUrl = "https://api.github.com/search/repositories?q=" + language + "is:featured&sort=help-wanted-issues";
+
+ fetch(apiUrl).then(function(response){
+     if(response.ok){
+         response.json().then(function(data){
+             displayRepos(data.items, language);
+         });
+     }
+     else{
+         alert("Error: " + response.statusText);
+     }
+ });
+};
+
+let buttonClickHandler = function(event){
+    let language = event.target.getAttribute("data-language");
+    if(language){
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+};
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
